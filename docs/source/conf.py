3329cec25e95876297f68182514aea69756447b2
pizza1016 <10370007-pizza1016@users.noreply.gitlab.com>
2025-03-13 19:51:02 +1100

    created docs; consequential changes to docstrings, fixed type hints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path('../..').resolve()))
+sys.path.insert(0, str(Path('..', '..').resolve()))
 
 # Configuration file for the Sphinx documentation builder.
 #
